73cc2a28a33b2cda4a2609ed1dbfd4d117c0a6e8
Nyasha Karata <veximagames@gmail.com>
2025-12-20 13:06:13 +0200

    save

--- app/config/api.config.ts
+++ app/config/api.config.ts
@@ -1,4 +1,4 @@
 // @ts-nocheck
 export const API_CONFIG = {
-  baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://khaya-server.vercel.app/api',
+  baseUrl: 'https://khaya-server.vercel.app/api',
 } as const; 
